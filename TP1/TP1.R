cc9f270bbc1c8cda5a9b4e10f58745dc100b5704
Anne-Danielle5 <123992139+Anne-Danielle5@users.noreply.github.com>
2023-01-31 15:24:50 +0100

    Update TP1.R

--- TP1/TP1.R
+++ TP1/TP1.R
@@ -7,6 +7,20 @@ brutToNet<- function (salairebrutmensuel) {
     }
   return (calcul) }
 
-brutToNet2<- function(salairebrutmensuel,statcontrat)
+
+#2.1.2
+brutToNet2<- function(salairebrutmensuel1,statcontrat){
+  calcul<- salairebrutmensuel1*0.75
+  if (is.numeric(salairebrutmensuel1) & statcontrat=="cadre"| statcontrat="non cadre"){
+  if (statcontrat="cadre"){
+    net=salairebrutmensuel1-salairebrutmensuel1*0.25
+    netaprsimpot=net-net*(1-0.075)
+    return(netaprsimpot)
+  }else
+    net= salairebrutmensuel1-salairebturmensuel1*0.22
+    netaprsimpot=net-net*(1-0.075)
+    return(netaprsimpot)
+  }
+
 
 
